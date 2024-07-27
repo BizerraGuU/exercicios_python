c643a65f6846db6073746e9d379e4eb0c9c7e839
Gustavo Bezerra <g.bezerra.contato@gmail.com>
2024-07-27 14:55:19 -0300

    exercicio 1 finalizado

--- ListasExercicios/Lista1/Resolução/Ex1/Baskara.py
+++ ListasExercicios/Lista1/Resolução/Ex1/Baskara.py
@@ -1,11 +1,14 @@
+import math
+
 class Baskara:
-    def __init__(self, coeficienteA=1, coeficienteB=0, coeficienteC=0):
+    def __init__(self, coeficienteA:int=1, coeficienteB:int=0, coeficienteC:int=0):
         self.__coeficienteA = coeficienteA
         self.__coeficienteB = coeficienteB
         self.__coeficienteC = coeficienteC
+        self.__raizes = []
 
     @property
-    def coeficienteA(self):
+    def coeficienteA(self): 
         return self.__coeficienteA
 
     @coeficienteA.setter
@@ -28,13 +31,39 @@ class Baskara:
     def coeficienteC(self, coeficienteC):
         self.__coeficienteC = coeficienteC
     
-    def print(self):
-        print(f"{self.__coeficienteA}x² + {self.__coeficienteB}x + {self.__coeficienteC}")
-
+    @property
+    def raizes(self):
+        return self.__raizes
+    
     def calculo(self):
         
+        delta = ((self.__coeficienteB**2) - (4*(self.__coeficienteA * self.__coeficienteC)))
+       
+        if delta < 0:
+            return "raiz negativa"
         
-        #(-b±√(b²-4ac))/(2a) .
+        else: 
+            raiz_delta = math.sqrt(delta)
 
-    def raizes(self):
-        pass
+            x_um = (-(self.__coeficienteB) + raiz_delta)/(2*self.__coeficienteA)
+            x_dois = (-(self.__coeficienteB) - raiz_delta)/(2*self.__coeficienteA)
+            
+            raiz_um = round(x_um, 2)
+            raiz_dois = round(x_dois, 2)
+
+            self.__raizes.extend([raiz_um, raiz_dois])
+            
+            print(f"O valor de X1 e :{raiz_um} e o valor de X2 e :{raiz_dois}")
+        
+    def raiz(self):
+        for i in self.__raizes:
+            j = round(i, 2)
+            print(f"{j}")
+
+    def print_equacao(self):
+        print(f"{self.__coeficienteA}x² + {self.__coeficienteB}x + {self.__coeficienteC}")
+
+    def __str__(self):
+        raizes_str = ', '.join(map(str, self.__raizes)) if self.__raizes else "Nenhuma raiz calculada"
+        return (f"Equação: {self.__coeficienteA}x² + {self.__coeficienteB}x + {self.__coeficienteC}\n"
+                f"Raízes: {raizes_str}")
---
+++ ListasExercicios/Lista1/Resolução/Ex1/Main.py
@@ -0,0 +1,8 @@
+from Baskara import Baskara
+
+class Main:
+    if __name__ == "__main__":
+        eq1 = Baskara(1, 6, 2)
+        eq1.calculo()
+        eq1.print_equacao()
+        eq1.raiz()
